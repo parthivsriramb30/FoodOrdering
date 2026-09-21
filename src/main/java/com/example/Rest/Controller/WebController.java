10a4ef0f0f7ae911e0116c642ec22110e8f20114
parthivsriramb30 <parthivsriram.b2025cse@sece.ac.in>
2026-09-21 19:21:27 +0530

    ui and db connect

---
+++ src/main/java/com/example/Rest/Controller/WebController.java
@@ -0,0 +1,13 @@
+package com.example.Rest.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping({"/", "/home"})
+    public String index() {
+        return "forward:/index.html";
+    }
+}
